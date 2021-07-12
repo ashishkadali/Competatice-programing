6b5dc46431056988ef87d7ecf4467e0b9aa0913a
ashishkadali <ashishkadali7@msitprogram.net>
2021-07-12 17:28:12 +0530

    distance

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -3,7 +3,15 @@
 # that represent the two points (x1, y1) and (x2, y2), 
 # and returns the distance between those points as a int.
 
-
+import math
 def fun_distance(x1, y1, x2, y2):
 	# your code goes here
-	return 0
+	# a=math.sqrt((((x2-x1)**2)+((y2-y1)**2))*0.5)
+	a=(x2-x1)**2
+	b=(y2-y1)**2
+
+
+	return math.floor( math.sqrt(a+b))
+
+	# 3.5=3 /floor
+	# 3.5=4 / ceil
