7d915c08f33169b90c5af94309f6045f2b0e756f
ashishkadali <ashishkadali7@msitprogram.net>
2021-07-12 17:11:29 +0530

    ismultiple solution

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -4,6 +4,15 @@
 # Also, you should make constructive use of the isFactor function you just wrote above.
 
 
-
 def fun_ismultiple(m, n):
+	if m==n:
+		return True
+	elif m==0:
+		return True
+	elif n==0:
+		return False
+	elif m%n==0:
+		return True
+
+
 	return False # replace with your solution
